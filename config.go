c63b9f70e3a30d1ca6dee99615bb88d37638346e
xank <xank@qq.com>
2017-12-31 01:59:14 +0800

    rename

--- config.go
+++ config.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"os"
+)
+
+func getString(name string, def interface{}) string {
+	val := os.Getenv(name)
+	if val == "" && def == nil {
+		panic(name)
+	}
+
+	if val != "" {
+		return val
+	}
+	return def.(string)
+}
+
 // 服务器配置
 var (
-	mysqlUrl         = getString("MYSQL_URL", nil),
-	redisUrl         = getString("REDIS_URL", nil)
+	mysqlUrl         = getString("MYSQL_URL", "http://127.0.0.1")
+	redisUrl         = getString("REDIS_URL", "http://127.0.0.1")
 )
--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Sprintf("17Disney ETL Service")
+	fmt.Println("17Disney ETL Service")
 }
