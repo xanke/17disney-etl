8ba13060acf3b217ea87545feca48d8e8e29801d
xank <xank@qq.com>
2018-01-08 00:31:04 +0800

    1

--- webserver/main.go
+++ webserver/main.go
@@ -4,22 +4,13 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os/exec"
 	"log"
 )
 
-func reLaunch() {
-	cmd := exec.Command("sh", "./deploy.sh")
-	err := cmd.Start()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = cmd.Wait()
-}
 
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1>17Disney ETL</h1>")
+	io.WriteString(w, "<h1>17Disney ETL2</h1>")
 	reLaunch()
 }
 
