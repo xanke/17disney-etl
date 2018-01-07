1fba59b0f555984a808834617238e12ab062bfd7
xank <xank@qq.com>
2018-01-08 00:41:33 +0800

    dev

--- webserver/main.go
+++ webserver/main.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"log"
 )
 
 
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "<h1>17Disney ETL2</h1>")
-	reLaunch()
 }
 
 func main() {
